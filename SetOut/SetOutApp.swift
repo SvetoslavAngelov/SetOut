88d0d8e69d13fa622ee486c3bfde681276c377b7
Svetoslav Angelov <svetoslavangelov86@gmail.com>
2023-05-12 12:56:36 +0900

    Initial commit

---
+++ SetOut/SetOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetOutApp.swift
+//  SetOut
+//
+//  Created by Svetoslav Angelov on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SetOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
